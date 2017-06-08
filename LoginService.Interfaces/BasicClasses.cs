360dfdf2d06031e53ae7f673c1414acf350233c9
Shkire <danielsmash94@hotmail.com>
2017-06-08 19:47:33 +0200

    LoginService.Interfaces updated
    +LoginService.Interfaces.BasicClasses created
    +Code commented and explained

---
+++ LoginService.Interfaces/BasicClasses.cs
@@ -0,0 +1,46 @@
+﻿namespace LoginService.Interfaces.BasicClasses
+{
+    /// <summary>
+    /// Contains the definition of a game session
+    /// </summary>
+    public class GameDefinition
+    {
+        /// <summary>
+        /// Name of the game session
+        /// </summary>
+        public string id;
+
+        /// <summary>
+        /// Maximum number of players in the game session
+        /// </summary>
+        public int maxPlayers;
+
+        /// <summary>
+        /// Number of players in the game session
+        /// </summary>
+        public int players;
+
+        /// <summary>
+        /// Map ID of the game session
+        /// </summary>
+        public int map;
+
+        /// <summary>
+        /// Empty constructor
+        /// </summary>
+        public GameDefinition()
+        {
+        }
+
+        /// <summary>
+        /// Creates a new GameDefinition
+        /// </summary>
+        /// <param name="i_gameId">ID of the game session</param>
+        /// <param name="i_maxPlayers">Maximim number of players in the game session</param>
+        public GameDefinition(string i_gameId, int i_maxPlayers)
+        {
+            id = i_gameId;
+            maxPlayers = i_maxPlayers;
+        }
+    }
+}
--- LoginService.Interfaces/ILoginService.cs
+++ LoginService.Interfaces/ILoginService.cs
@@ -1,45 +1,60 @@
-﻿using Microsoft.ServiceFabric.Services.Remoting;
-using System;
+﻿using LoginService.Interfaces.BasicClasses;
+using Microsoft.ServiceFabric.Services.Remoting;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace LoginService.Interfaces
 {
-    public class GameInfo
-    {
-        public string id;
-        public int maxPlayers;
-        public int players;
-        public int map;
-
-        public GameInfo()
-        {
-        }
-
-        public GameInfo(string i_gameId, int i_maxPlayers)
-        {
-            id = i_gameId;
-            maxPlayers = i_maxPlayers;
-        }
-    }
-
+    /// <summary>
+    /// LoginService method declaration. Allows comunication with LoginService from client or other services
+    /// </summary>
     public interface ILoginService: IService
     {
+        /// <summary>
+        /// Tries to log in player to the server
+        /// </summary>
+        /// <param name="i_player">Player name</param>
+        /// <param name="i_pass">Player password</param>
+        /// <returns>True if player was able to be logged in, false otherwise</returns>
         Task<bool> Login(string i_player, string i_pass);
 
+        /// <summary>
+        /// Creates a new player on the server
+        /// </summary>
+        /// <param name="i_player">Player name</param>
+        /// <param name="i_pass">Player password</param>
+        /// <returns>True if player was able to be created, false otherwise</returns>
         Task<bool> CreatePlayer(string i_player, string i_pass);
 
-        Task<bool> CreateGameAsync(GameInfo i_info);
+        /// <summary>
+        /// Creates a new game session on the server
+        /// </summary>
+        /// <param name="i_gameDef">Game session definition</param>
+        /// <returns>True if game session was able to be created, false otherwise</returns>
+        Task<bool> CreateGameAsync(GameDefinition i_gameDef);
 
+        /// <summary>
+        /// Increases in 1 the player counter of the game session (SQL register)
+        /// </summary>
+        /// <param name="i_gameId">Game session ID</param>
         Task AddPlayerAsync(string i_gameId);
 
+        /// <summary>
+        /// Decreases in 1 the player counter of the game session (SQL register)
+        /// </summary>
+        /// <param name="i_gameId">Game session ID</param>
         Task RemovePlayerAsync(string i_gameId);
 
-        Task DeleteGame(string i_id);
+        /// <summary>
+        /// Deletes a game session on the server (SQL register)
+        /// </summary>
+        /// <param name="i_gameId">Game session ID</param>
+        Task DeleteGame(string i_gameId);
 
-        Task<List<GameInfo>> GetGameList();
+        /// <summary>
+        /// Returns a list with all game session definitions on the server
+        /// </summary>
+        Task<List<GameDefinition>> GetGameList();
 
     }
 }
